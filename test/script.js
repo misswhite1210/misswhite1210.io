c26f88b92f12f4c18b99fa54bb6bc16be1be6c6f
leier <2846729161@qq.com>
2025-05-05 15:36:26 +0800

    Update script.js

--- test/script.js
+++ test/script.js
@@ -53,42 +53,35 @@ noButton.addEventListener("click", () => {
 const loveTest = `!!!上号!! ( >᎑<)♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
-
-
 yesButton.addEventListener("click", function () {
-  // 先创建基础 HTML 结构
+  // 替换整个 body 内容
   document.body.innerHTML = `
-        <div class="yes-screen">
-            <h1 class="yes-text"></h1>
-            <img src="images/OIP.jpg" alt="自嘲熊" class="yes-image">
-        </div>
-    `;
+    <div class="yes-screen" style="text-align:center; padding: 40px;">
+      <h1 class="yes-text" style="font-size: 2em; margin-bottom: 20px;"></h1>
+      <img src="images/OIP.jpg" alt="自嘲熊" class="yes-image" style="max-width: 100%; height: auto;">
+    </div>
+  `;
 
-  // 确保用户名安全地插入
+  // 设置文本内容
   document.querySelector(".yes-text").innerText = loveTest;
 
-  // 禁止滚动，保持页面美观
+  // 禁止滚动
   document.body.style.overflow = "hidden";
 });
 
 
+// yesButton.addEventListener("click", function () {
+//   // 先创建基础 HTML 结构
+//   document.body.innerHTML = `
+//         <div class="yes-screen">
+//             <h1 class="yes-text"></h1>
+//             <img src="images/OIP.jpg" alt="自嘲熊" class="yes-image">
+//         </div>
+//     `;
 
-// yesButton.addEventListener("click", () => {
-//   // 重置所有动画样式
-//   yesButton.style.transform = "scale(1)";
-//   noButton.style.transform = "translateX(0)";
-//   mainImage.style.transform = "translateY(0)";
-//   questionText.style.transform = "translateY(0)";
+//   // 确保用户名安全地插入
+//   document.querySelector(".yes-text").innerText = loveTest;
 
-//   // 把当前显示的gif替换成OIP.jpg
-//   mainImage.src = `images/OIP.jpg?t=${Date.now()}`;
-
-//   // 修改文字内容
-//   questionText.innerText = "你选择了“可以”，谢谢你！";
-
-//   // 重置点击计数，防止后续“不要”按钮异常
-//   clickCount = 0;
-
-//   // 重置“不要”按钮文字
-//   noButton.innerText = "不要";
+//   // 禁止滚动，保持页面美观
+//   document.body.style.overflow = "hidden";
 // });
