e4a5428570fdc5bbbd3ff3c5d8aa9f6fb5e0cb2e
leier <2846729161@qq.com>
2025-05-12 16:05:34 +0800

    Update script.js

--- test/script.js
+++ test/script.js
@@ -58,18 +58,19 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// const loveTest = `!!!上号!! ( >᎑<)♡︎ᐝ  ${
-//   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
-// }`;
-// yesButton.addEventListener("click", function () {
-//   // 替换整个 body 内容
-//   document.body.innerHTML = `
-//     <div class="yes-screen" style="text-align:center; padding: 40px;">
-//       <h1 class="yes-text" style="font-size: 2em; margin-bottom: 20px;"></h1>
-//       <img src="images/OIP.jpg" alt="自嘲熊" class="yes-image" style="max-width: 100%; height: auto;">
-//     </div>
-//   `;
-//   console.log("页面加载完成");
+const loveTest = `!!!上号!! ( >᎑<)♡︎ᐝ  ${
+  username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
+}`;
+yesButton.addEventListener("click", function () {
+  console.log("页面加载完成");
+  // 替换整个 body 内容
+  document.body.innerHTML = `
+    <div class="yes-screen" style="text-align:center; padding: 40px;">
+      <h1 class="yes-text" style="font-size: 2em; margin-bottom: 20px;"></h1>
+      <img src="images/OIP.jpg" alt="自嘲熊" class="yes-image" style="max-width: 100%; height: auto;">
+    </div>
+  `;
+
 
 //   // 设置文本内容
 //   document.querySelector(".yes-text").innerText = loveTest;
